40117cf32f8203e17e7b48c5a60274cbded6d1f9
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-05-06 23:57:22 -0500

    Creating Color Model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
-class Colors(models.Model):
-    name = models.CharField(blank=True, unique=True, max_length= 40)
-    hex = models.CharField(max_length=7, unique=True)
+class Color(models.Model):
+    name = models.CharField(blank=False, unique=True, max_length=40)
+    hexadecimal = models.CharField(max_length=7, unique=True)
+    red = models.PositiveSmallIntegerField()
+    green = models.PositiveSmallIntegerField()
+    blue = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
